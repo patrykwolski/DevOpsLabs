5c43b874bb311c27b90286a6039c792fd654b773
patrykwolski <patryk@24nettbutikk.no>
2021-05-16 23:25:54 +0200

    Update main.tf

--- terraform/accessing-variables/main.tf
+++ terraform/accessing-variables/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test-instance" {
   ami                    = "ami-00f69856ea957jgjf"
   instance_type          = "t2.nano"
   availability_zone      = "eu-central-1a"
-  subnet_id              = ["subnet-45e01539"]
+  subnet_id              = "subnet-45e01539"
   vpc_security_group_ids = ["sg-082d44eee7e8087ad","sg-aea68bc8"]
   associate_public_ip_address = true
   root_block_device {
