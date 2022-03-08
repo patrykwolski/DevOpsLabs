ba0ea9947dee1e6e7f9fd693fddb9186b7c8b2bc
patrykwolski <patryk@24nettbutikk.no>
2022-03-08 13:15:11 +0100

    Update 3-ec2.tf

--- terraform/3-ec2.tf
+++ terraform/3-ec2.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "test-instance-1" {
-  ami           = "ami-0a4f7079eb2559d2d"
+  ami           = "ami-0612e041366af94b2"
   instance_type = "t2.nano"
 }
 
 resource "aws_instance" "test-instance-2" {
-  ami           = "ami-0a4f7079eb2559d2d"
+  ami           = "ami-0612e041366af94b2"
   instance_type = "t2.nano"
 }
 
 resource "aws_instance" "test-instance-3" {
-  ami           = "ami-0a4f7079eb2559d2d"
+  ami           = "ami-0612e041366af94b2"
   instance_type = "t2.nano"
 }
