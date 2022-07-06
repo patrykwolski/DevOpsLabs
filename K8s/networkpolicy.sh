3d8c0030ea4a0ac77ff5218c6195a02b8744b727
patrykwolski <patryk@24nettbutikk.no>
2022-07-06 18:21:46 +0200

    Update networkpolicy.sh

--- K8s/networkpolicy.sh
+++ K8s/networkpolicy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 minikube delete
-minikube start --vm-driver none --network-plugin=cni
+minikube start --vm-driver=none --network-plugin=cni --extra-config=kubelet.housekeeping-interval=10s
 sudo mount bpffs -t bpf /sys/fs/bpf
-kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.6/install/kubernetes/quick-install.yaml
-kubectl get pods -n kube-system
+kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.9/install/kubernetes/quick-install.yaml
