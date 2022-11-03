cb3247cc41ce9d4b46a6cc85c7fa47b4ba92910b
patrykwolski <patryk@24nettbutikk.no>
2022-11-03 16:17:38 +0100

    Update main.tf

--- terraform/taint/main.tf
+++ terraform/taint/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "0.14.6"
+  required_version = "1.3.4"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.37"
+      version = "4.37"
     }
   }
 }
