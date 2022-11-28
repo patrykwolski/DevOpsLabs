0c13e01a37b189ace8330ed17cb609b829e5282b
patrykwolski <patryk@24nettbutikk.no>
2022-11-28 08:26:07 +0100

    Create 3-azure-instances.tf

---
+++ terraform/3-azure-instances.tf
@@ -0,0 +1,73 @@
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "$NICK-vm1"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_DS1_v2"
+  admin_username                  = "$NICK"
+  admin_password                  = "$NICK-1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  
+  resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "$NICK-vm2"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_DS1_v2"
+  admin_username                  = "$NICK"
+  admin_password                  = "$NICK-1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+    
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "$NICK-vm3"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_DS1_v2"
+  admin_username                  = "$NICK"
+  admin_password                  = "$NICK-1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
